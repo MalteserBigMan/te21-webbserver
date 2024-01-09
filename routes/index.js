80657195007adc543cb88f9cf595451ce6671aa7
malte <malte.lindkvist@elev.ga.ntig.se>
2024-01-09 11:15:09 +0100

    The one piece

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,8 @@ res.render('index.njk', { title: 'Välkommen till Keros Kizzeria' })
 router.get("/about", function (req, res) {
   res.render('about.njk', { title: 'Keros Kizzeria' })
 })
+
+router.get("/onepiece", function (req, res) {
+  res.render('onepiece.njk', { title: 'THE ONE PIECE IS REAL' })
+})
 module.exports = router
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express")
+const bodyParser = require('body-parser')
 const nunjucks = require('nunjucks');
 const app = express();
 const indexRouter = require("./routes/index")
-
+const xmasRouter = require('./routes/xmas')
 
 nunjucks.configure('views', {
     autoescape: true,
@@ -13,12 +14,17 @@ nunjucks.configure('views', {
 const port = process.env.PORT || 3000
 
 
+
 app.use((req, res, next)=>{
     res.locals.url = req.originalUrl
     next()
 })
 
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
 app.use("/", indexRouter)
+app.use('/xmas', xmasRouter)
 
 app.use(express.static('public'))
 
